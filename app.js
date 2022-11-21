77e470dca5a4c23a3f75b69173ba37ec294257f1
shipunova <shipunovamozhet@mail.ru>
2022-11-21 15:14:06 +0300

    задачка возраст

--- app.js
+++ app.js
@@ -1,9 +1,19 @@
-const age=15;
+const age=18alala;
 const gender='Male' //female
 
-if (age>18) {
+age=+age;
+
+if (!isNaN(age)) {if (age>18) {
     console.log("Вам больше 18 лет")
-};
+} else if (age<18) {
+    console.log('Вам меньше 18 лет')
+} else if (age===18){
+    console.log('Вам 18 лет')
+} else {
+    console.log("Укажите Ваш возраст")
+}}
+
+
 
 if (gender.toLocaleLowerCase==='male') {
     console.log("Вам налево")
