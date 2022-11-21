5fc65a4de60e05592faf1a93c8b7384cd730758c
shipunova <shipunovamozhet@mail.ru>
2022-11-21 14:44:52 +0300

    добавили условие регистра

---
+++ app.js
@@ -0,0 +1,15 @@
+const age=15;
+const gender='Male' //female
+
+if (age>18) {
+    console.log("Вам больше 18 лет")
+};
+
+if (gender.toLocaleLowerCase==='male') {
+    console.log("Вам налево")
+}
+
+if (gender.toLocaleLowerCase==='female') {
+    console.log("Вам направо")
+}
+
